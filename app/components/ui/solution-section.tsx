3daefa320b71361f0573531f61068268c8f73281
nandju <nlbmd17@gmail.com>
2024-12-03 18:53:24 +0100

    function finish

--- app/components/ui/solution-section.tsx
+++ app/components/ui/solution-section.tsx
@@ -25,20 +25,20 @@ export default function Solution(){
     
     return(
         <div className="bg-white">
-            <div className="flex flex-col justify-center text-center gap-3">
+            <div className="flex flex-col justify-center text-center gap-3 m-3">
                 <div className="font-mono text-5xl font-thin">Nos Solution</div>
                 <div className="font-roboto text-lg">Découvrez une gamme complète de services adaptés à vos besoins : location de camions, transport de marchandises, travaux publics, et bien plus. Avec ETS Global Transport, bénéficiez de solutions fiables, économiques et sur mesure pour simplifier votre logistique.</div>
             </div>
-            <div className="flex flex-row justify-around gap-4 bg-gray-200 p-6 rounded-3xl m-8">
-                <div className="flex flex-col ">
-                    <img src={solution_items.image} alt={solution_items.titleimage} />
+            <div className="flex flex-row justify-around gap-4 bg-gray-200 p-6 rounded-3xl m-8 relative">
+                <div className="flex flex-col justify-start items-start gap-4">
+                    <img className="rounded-3xl h-60" src={solution_items.image} alt={solution_items.titleimage} />
                     <div>
-                        <div>{solution_items.title}</div>
+                        <div className="font-mono font-semibold">{solution_items.title}</div>
                         {showMore && <p>{solution_items.subtitle}</p> }
                     </div>
-                    <button className="bg-gray-200 text-black p-2 rounded-xl hover:bg-white" onClick={handleMoreClick}>En savoir {showMore ? 'moins' : 'plus'}</button>
+                    <button className="bg-gray-200 text-black p-2 rounded-xl border border-white hover:bg-white" onClick={handleMoreClick}>En savoir {showMore ? 'moins' : 'plus'}</button>
                 </div>
-                <button onClick={handleNextClick}>Suivant</button>
+                <button className="bg-gray-200 text-black p-2 rounded-xl border border-white  hover:bg-white h-28 w-20 relative my-32" onClick={handleNextClick}>Suivant</button>
             </div>
         </div>
     )
