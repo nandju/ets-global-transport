ae8e2b7b118ea48c49953a15834e9c7e5593465a
nandju <nlbmd17@gmail.com>
2024-12-18 12:59:00 +0100

    responsive continue

--- components/ui/testimonial-section.tsx
+++ components/ui/testimonial-section.tsx
@@ -4,10 +4,10 @@ import { NumberTicker } from "./numberticker";
 export default function Testimonial(){
     return(
         <div className="flex flex-col md:flex-row justify-around bg-muted p-20">
-            <div className="flex flex-col justify-start items-center md:items-start text-center md:text-start gap-8">
+            <div className="flex flex-col justify-between md:justify-start md:items-start text-center md:text-start gap-8">
                 <div className="text-3xl md:text-5xl font-semibold">Nos chiffres clés</div>
-                <div className="text-gray-600 dark:text-gray-200 text-lg md:text-xl">Des résultats qui témoignent de notre engagement et de notre <br /> expertise dans le secteur du transport et des travaux publics.</div>
-                <Button color="primary" className="py-2 px-4">En savoir plus</Button>
+                <div className="text-gray-600 dark:text-gray-200 md:text-lg lg:text-xl text-center md:text-start sm:my-2 my-0 lg:my-2">Des résultats qui témoignent de notre engagement et de notre <br /> expertise dans le secteur du transport et des travaux publics.</div>
+                <Button color="primary" className="mt-4">En savoir plus</Button>
             </div>
             <div className="flex flex-col md:grid md:grid-cols-2 pt-6 gap-8">
                 <div className="flex flex-col justify-center text-primary text-center bg-card border dark:border-primary scale-105 cursor-pointer rounded-lg p-8">
